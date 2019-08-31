41e4f27fc8334ac2fcb08e3104ef12133c3e2bb0
shredderzwj <shredderzwj@hotmail.com>
2019-08-31 14:22:55 +0800

    2.7 opt 5

--- app.py
+++ app.py
@@ -1,18 +1,33 @@
 # -*- coding: utf-8 -*-
 
 
-from flask import Flask, render_template, g, session, url_for, request
+from flask import Flask, render_template, session, url_for, request, redirect
 from flask_script import Manager
 import re
 import base64
+import hashlib
+from collections import defaultdict
+
 
 from orm import ExpressionWordsOperate
 from parse_news import Parse
 
 
 app = Flask(__name__, template_folder='templates', static_folder='static')
-app.secret_key = "klsajdghgkljasglkhasdfhasdgasdfg"
+app.secret_key = "==30=05=/2@#sdf%#f@$a-sd@!##$@#$8!@#$!0@%!@#sdf21fg5sd44fa23"
 manager = Manager(app)
+user = 'alpha_zero'
+pwd = '978ff1afe3a13140a9c7193d6f11d29030f06238043a5c646e293e6be3cb570b'
+
+
+def check_login(f):
+    def wrap(*args, **kwargs):
+        if session.get('user') == user and session.get('pwd') == pwd:
+            return f(*args, **kwargs)
+        else:
+            return redirect(url_for('login'))
+    wrap.__name__ = f.__name__    # 将传入函数的 __name__ 属性值赋值给闭包函数，以便 url_for 函数捕获地址。
+    return wrap
 
 
 @app.before_first_request
@@ -54,8 +69,29 @@ def extra():
 
 
 @app.route('/expression-words-operate', methods=['GET', 'POST'])
+@check_login
 def expression_words_operate():
-    pass
+    operate = ExpressionWordsOperate()
+    info = defaultdict(list)
+    if request.method == "POST":
+        words = request.form['s'].strip().split(' ')
+        words = [word for word in words if len(word) < 5]
+        bools = operate.add_words(words)
+        words_added = [word for i, word in enumerate(words) if bools[i]]
+        if words_added:
+            info['add'] = words_added
+    else:
+        try:
+            _id = int(request.args.get("del"))
+            word = operate.del_word_by_id(_id)
+            print(_id, word)
+            if word:
+                info['delete'].append(word)
+        except:
+            pass
+    print(info)
+    words_dict = operate.get_all()
+    return render_template('manager.html', words=words_dict, info=info)
 
 
 @app.route('/fly-words')
@@ -71,6 +107,31 @@ def fly_words():
     if not fly_str:
         fly_str = "没有内容"
     return render_template('fly_words.html', **get_fly_words_list(fly_str))
+
+
+@app.route('/login', methods=["POST", "GET"])
+def login():
+    if request.method == 'POST':
+        user_get = request.form['user']
+        pwd_get = request.form['pwd']
+        pwd_hash = hashlib.sha256(pwd_get.encode()).hexdigest()
+        if user == user_get and pwd == pwd_hash:
+            session['user'] = user
+            session['pwd'] = pwd_hash
+            return redirect(url_for("expression_words_operate"))
+        else:
+            return redirect(request.url)
+    else:
+        if session.get('user') == user and session.get('pwd') == pwd:
+            return redirect(url_for("expression_words_operate"))
+        return render_template('login.html')
+
+
+@app.route('/logout')
+def logout():
+    session['user'] = ""
+    session['pwd'] = ""
+    return redirect(url_for('login'))
 
 
 def get_fly_words_list(fly_str):
--- orm.py
+++ orm.py
@@ -8,7 +8,8 @@ from conf import DataBase
 
 class ExpressionWords(DataBase.Base):
     __tablename__ = 'expression_words'
-    word = Column(String(20), primary_key=True)
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    word = Column(String(20))
 
     def to_dict(self):
         return {x.name: getattr(self, x.name) for x in self.__table__.columns}
@@ -23,28 +24,44 @@ class ExpressionWordsOperate(DataBase):
 
     def __call__(self, number=None):
         if number is None:
-            query = self.session.query(ExpressionWords)
+            query = self.__query_base()
         else:
-            query = self.session.query(ExpressionWords).limit(number)
+            query = self.__query_base().limit(number)
         return self.__get_data(query)
 
     def __getitem__(self, item):
-        query = self.session.query(ExpressionWords)[item]
+        query = self.__query_base()[item]
         return self.__get_data(query)
 
     def __iter__(self):
-        for x in self.session.query(ExpressionWords):
+        for x in self.__query_base():
             yield x.word
 
+    def __query_base(self):
+        return self.session.query(ExpressionWords)
+    
     def __get_data(self, query):
         infos = []
         for x in query:
             infos.append(x.word)
         return infos
 
+    def get_all(self):
+        infos = []
+        for x in self.__query_base().order_by(text('-id')):
+            infos.append(x.to_dict())
+        return infos
+
     def is_word_exists(self, word):
         """判断词是否存在数据库中"""
         query = self.session.query(ExpressionWords).filter(ExpressionWords.word == word)
+        return self.session.query(
+            query.exists()
+        ).scalar()
+
+    def is_word_exists_by_id(self, _id):
+        """判断词是否存在数据库中"""
+        query = self.session.query(ExpressionWords).filter(ExpressionWords.id == _id)
         return self.session.query(
             query.exists()
         ).scalar()
@@ -55,7 +72,7 @@ class ExpressionWordsOperate(DataBase):
         :param word: str 词
         :return: 添加成功返回 True， 否则返回 None，一般是由于词库中已有此词
         """
-        if not self.is_word_exists(word):
+        if word and (not self.is_word_exists(word)):
             self.session.add(ExpressionWords(word=word))
             self.session.commit()
             return True
@@ -71,7 +88,7 @@ class ExpressionWordsOperate(DataBase):
         if isinstance(words, (list, tuple, set)):
             opera_bools = []
             for word in words:
-                if not self.is_word_exists(word):
+                if word and (not self.is_word_exists(word)):
                     self.session.add(ExpressionWords(word=word))
                     opera_bools.append(True)
                 else:
@@ -94,3 +111,17 @@ class ExpressionWordsOperate(DataBase):
         else:
             return None
 
+    def del_word_by_id(self, _id):
+        """
+        删除一个词
+        :param _id: int 要删除的词的id
+        :return: 删除成功返回 删除的词。否则返回 None， 一般是由于数据库中不存在此词。
+        """
+        if self.is_word_exists_by_id(_id):
+            query = self.session.query(ExpressionWords).filter(ExpressionWords.id == _id)
+            word = query.first().word
+            query.delete()
+            self.session.commit()
+            return word
+        else:
+            return None
